7d10168831391c5678131ce2050628e6fe5de313
Namith Scindhia <yagamilight1987@gmail.com>
2024-09-17 00:00:32 +0530

    added styles and html.

---
+++ public/js/script.js
@@ -0,0 +1,27 @@
+window.addEventListener('DOMContentLoaded', () => {
+  const themeSelectorButton = document.querySelector('.theme-selector');
+
+  // On page load or when changing themes, best to add inline in `head` to avoid FOUC
+  if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    document.documentElement.classList.add('dark');
+    localStorage.theme = 'dark';
+    themeSelectorButton.setAttribute('data-theme', 'dark');
+  } else {
+    document.documentElement.classList.remove('dark');
+    localStorage.theme = 'light';
+    themeSelectorButton.setAttribute('data-theme', 'light');
+  }
+
+  themeSelectorButton.addEventListener('click', () => {
+    const hasDarkClass = document.documentElement.classList.contains('dark');
+    if (!hasDarkClass) {
+      document.documentElement.classList.add('dark');
+      localStorage.theme = 'dark';
+      themeSelectorButton.setAttribute('data-theme', 'dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+      localStorage.theme = 'light';
+      themeSelectorButton.setAttribute('data-theme', 'light');
+    }
+  });
+});
